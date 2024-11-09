625c0f64b63f3816e55a966e9883df6aa71e2f86
weslleysilv4 <weslleysilva.swe@gmail.com>
2024-11-08 23:41:48 -0300

    feat: Update font styles, remove unused React SVG, and add HeroSection component to Home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,11 @@
-import React from "react";
 import Navbar from "@Components/Navbar";
+import HeroSection from "./HeroSection";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <HeroSection />
     </div>
   );
 }
