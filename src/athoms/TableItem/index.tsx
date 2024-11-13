0ffac98396025aa9aebb52c0ea54d40a350b3b2f
Victor Gabriel <victor_tiss@outlook.com>
2024-11-13 19:54:56 -0300

    feat(tableItem): add table item props & move to athoms folder

---
+++ src/athoms/TableItem/index.tsx
@@ -0,0 +1,71 @@
+import { Star, CaretUp, CaretDown } from '@phosphor-icons/react';
+import ROW_SIZE from '../../components/Table/constants';
+import { useState } from 'react';
+import { Cryptocurrency } from '@Types/Cryptocurrency';
+
+export default function TableItem(props: Cryptocurrency) {
+  const [favorite, setFavorite] = useState<boolean>(false);
+
+  return (
+    <tr>
+      <td width={ROW_SIZE.X_SMALL} style={{ justifyContent: 'center' }}>
+        <Star
+          size={16}
+          weight={(favorite && 'fill') || 'regular'}
+          onClick={() => setFavorite(!favorite)}
+        />
+      </td>
+      <td width={ROW_SIZE.SMALL}>{props.market_cap_rank}</td>
+      <td
+        width={ROW_SIZE.X_LARGE}
+        style={{ justifyContent: 'flex-start', gap: 10 }}
+      >
+        <img src={props.image} alt="Coin image" />
+        <span>
+          {props.name}{' '}
+          <span
+            style={{
+              fontWeight: 400,
+              fontSize: 12,
+              color: '#343a40',
+            }}
+          >
+            {props.symbol.toUpperCase()}
+          </span>
+        </span>
+      </td>
+      <td width={ROW_SIZE.LARGE} style={{ justifyContent: 'end' }}>
+        {props.current_price.toLocaleString('en-US', {
+          style: 'currency',
+          currency: 'USD',
+        })}
+      </td>
+      <td
+        width={ROW_SIZE.MEDIUM}
+        style={{
+          justifyContent: 'end',
+          color: props.price_change_percentage_24h > 0 ? '#02C200' : '#C20000',
+        }}
+      >
+        {props.price_change_24h > 0 ? (
+          <CaretUp weight="fill" size={16} />
+        ) : (
+          <CaretDown weight="fill" size={16} />
+        )}
+        {props.price_change_percentage_24h.toFixed(2)}%
+      </td>
+      <td width={ROW_SIZE.LARGE} style={{ justifyContent: 'end' }}>
+        {props.market_cap_change_24h.toLocaleString('en-US', {
+          style: 'currency',
+          currency: 'USD',
+        })}
+      </td>
+      <td width={ROW_SIZE.LARGE} style={{ justifyContent: 'end' }}>
+        {props.market_cap.toLocaleString('en-US', {
+          style: 'currency',
+          currency: 'USD',
+        })}
+      </td>
+    </tr>
+  );
+}
