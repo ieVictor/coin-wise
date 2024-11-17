d895585155e6ccefb847c73c818870965bf3ebb6
weslleysilv4 <weslleysilva.swe@gmail.com>
2024-11-17 17:44:21 -0300

    feat: Add Footer component to CoinPrice page and update styles for links

--- src/pages/CoinPrice/index.tsx
+++ src/pages/CoinPrice/index.tsx
@@ -1,11 +1,13 @@
 import Navbar from "@Components/Navbar";
 import MainSection from "./MainSection";
+import Footer from "@Components/Footer";
 
 export default function CoinPrice() {
   return (
     <>
       <Navbar />
       <MainSection />
+      <Footer />
     </>
   );
 }
