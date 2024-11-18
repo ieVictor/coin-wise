6cee0191e107c795967be6ab8793ea0b40377afe
Victor Gabriel <victor_tiss@outlook.com>
2024-11-17 22:33:59 -0300

    feat(routes): add error route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,29 @@
-import { Route, Routes } from "react-router-dom";
-import Home from "@Pages/Home";
-import CoinPrice from "@Pages/CoinPrice";
-import SearchPage from "@Pages/SearchPage";
-import Error404Page from "@Pages/error404";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from '@Pages/Home';
+import CoinPrice from '@Pages/CoinPrice';
+import SearchPage from '@Pages/SearchPage';
+import Error404Page from '@Pages/error404';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+    errorElement: <Error404Page />,
+    children: [
+      {
+        path: 'coin/:id',
+        element: <CoinPrice />,
+      },
+      {
+        path: 'search',
+        element: <SearchPage />,
+      },
+    ],
+  },
+]);
 
 function Router() {
-  return (
-    <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/coin/:id" element={<CoinPrice />} />
-        <Route path="/search" element={<SearchPage />} />
-        <Route path="*" element={<Error404Page />} />
-      </Routes>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default Router;
