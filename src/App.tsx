9e969b9e19ac8d84e176253b76efe730c20c3cbd
Victor Gabriel <victor_tiss@outlook.com>
2025-02-07 21:43:09 -0300

    feat(auth): add loading handler

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,27 @@ import { FavoritesProvider } from '@Contexts/FavoritesProvider';
 import Router from './routes';
 import Login from '@Pages/Login';
 import useAuth from '@Services/useAuth';
+import { CircularProgress } from '@mui/material';
 
 function App() {
-  const { user } = useAuth();
-  if (!user.token) return <Login />;
+  const { accessToken, isLoading, user } = useAuth();
+  if (isLoading)
+    return (
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          height: '100vh',
+          width: '100%',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <CircularProgress />
+        <h1 style={{ fontSize: 32, color: '#1459f1' }}>CoinWise.</h1>
+      </div>
+    );
+  if (!accessToken || !user) return <Login />;
 
   return (
     <>
