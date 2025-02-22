944a7a2f526b0bf75aa3469aa34b4defa518d1c9
Victor Gabriel <victor_tiss@outlook.com>
2025-02-21 22:12:19 -0300

    feat(api): change auth route paths

--- src/config/API.ts
+++ src/config/API.ts
@@ -1,12 +1,12 @@
 export const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 export const apiUrl = {
-  LOGIN: `${API_BASE_URL}/login`,
-  LOGOUT: `${API_BASE_URL}/logout`,
+  LOGIN: `${API_BASE_URL}/auth/login`,
+  LOGOUT: `${API_BASE_URL}/auth/logout`,
   REFRESH_TOKEN: `${API_BASE_URL}/auth/refresh`,
   AUTH_CHECK: `${API_BASE_URL}/auth/check`,
   COIN_BY_ID: `${API_BASE_URL}/coins`,
-  COIN_BY_SEARCH: `${API_BASE_URL}/coins/search`,
+  COIN_BY_SEARCH: `${API_BASE_URL}/coins`,
   COINS_LIST: `${API_BASE_URL}/coins`,
   ADD_COIN: `${API_BASE_URL}/coins`,
 };
